51c16ea078f86616b7b75638b73587792b7a3fcb
ericnem <ericnem@outlook.com>
2023-07-02 12:46:23 -0400

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -36,7 +36,7 @@ public class Menu
         
         
         //Two player Button
-        JButton twoPlayer = new JButton("2 Player");
+        JButton twoPlayer = new JButton("2 Players");
         constraints.gridx = 0;
         constraints.gridy = 1;
         
